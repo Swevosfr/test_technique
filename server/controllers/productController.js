78a4e5a331ff359714bc6c827990e42a17ae8e76
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-07-11 13:44:00 +0200

    productController setup

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,19 +2,77 @@ const asyncHandler = require("express-async-handler");
 const Product = require("../models/product_model");
 
 const createProduct = asyncHandler(async (req, res) => {
-  res.json({ message: "Creation de produit" });
+  const { name, type, price, rating, warranty_years, available } = req.body;
+  if (!name || !type || !price || !rating || !warranty_years || !available) {
+    res.status(400);
+    throw new Error("Veuillez remplir tous les champs");
+  }
+  const product = await Product.create({
+    name,
+    type,
+    price,
+    rating,
+    warranty_years,
+    available,
+  });
+  console.log("Création du produit", product);
+  if (product) {
+    res.status(201).json(product);
+  } else {
+    res.status(400);
+    throw new Error("Les données du produit ne sont pas valides");
+  }
 });
 
 const getProduct = asyncHandler(async (req, res) => {
-  res.json({ message: "Lire le produit" });
+  const productId = req.params.id;
+  const product = await Product.findById(productId);
+  if (product) {
+    res.status(200).json(product);
+  } else {
+    res.status(404);
+    throw new Error("Produit introuvable");
+  }
 });
 
 const modifyProduct = asyncHandler(async (req, res) => {
-  res.json({ message: "Modifier le produit" });
+  const productId = req.params.id;
+  const { name, type, price, rating, warranty_years, available } = req.body;
+  if (!name || !type || !price || !rating || !warranty_years || !available) {
+    res.status(400);
+    throw new Error("Veuillez remplir tous les champs");
+  }
+  const updatedProduct = await Product.findByIdAndUpdate(
+    productId,
+    {
+      name,
+      type,
+      price,
+      rating,
+      warranty_years,
+      available,
+    },
+    { new: true }
+  );
+  console.log("Modification du produit", updatedProduct);
+  if (updatedProduct) {
+    res.status(200).json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Produit introuvable");
+  }
 });
 
 const deleteProduct = asyncHandler(async (req, res) => {
-  res.json({ message: "Suppression du produit" });
+  const productId = req.params.id;
+  const deletedProduct = await Product.findByIdAndDelete(productId);
+  console.log("Suppression du produit", deletedProduct);
+  if (deletedProduct) {
+    res.status(200).json({ message: "Produit supprimé" });
+  } else {
+    res.status(404);
+    throw new Error("Produit introuvable");
+  }
 });
 
 module.exports = { createProduct, getProduct, modifyProduct, deleteProduct };
--- server/models/product_model.js
+++ server/models/product_model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const productSchema = mongoose.Schema({
-  _id: { type: Number, required: true },
   name: { type: String, required: true },
   type: { type: String, required: true },
   price: { type: Number, required: true },
@@ -9,5 +9,7 @@ const productSchema = mongoose.Schema({
   warranty_years: { type: Number, required: true },
   available: { type: Boolean, required: true },
 });
+
+productSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Product", productSchema);
