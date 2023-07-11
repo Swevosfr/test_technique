044cfb92d102b0a9443e3a16465e683e7c142266
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-07-11 23:08:34 +0200

    Login and Register setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import PublicRoute from "../src/pages/public/PublicRoute";
+import UserRoute from "../src/pages/user/UserRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<PublicRoute />} />
+          <Route path="/user" element={<UserRoute />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
