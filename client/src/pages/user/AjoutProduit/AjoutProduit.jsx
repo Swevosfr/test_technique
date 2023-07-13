fbc8b5c45d1904a17dd7fb72e5f785ae8e74993b
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-07-13 15:51:01 +0200

    ajouter produit css change

--- client/src/pages/user/AjoutProduit/AjoutProduit.jsx
+++ client/src/pages/user/AjoutProduit/AjoutProduit.jsx
@@ -8,7 +8,7 @@ import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import AddIcon from "@mui/icons-material/Add";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
@@ -38,10 +38,10 @@ export default function SignUp() {
           }}
         >
           <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
+            <AddIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+            Ajouter un produit
           </Typography>
           <Box
             component="form"
@@ -53,11 +53,11 @@ export default function SignUp() {
               <Grid item xs={12} sm={6}>
                 <TextField
                   autoComplete="given-name"
-                  name="firstName"
+                  name="nom"
                   required
                   fullWidth
-                  id="firstName"
-                  label="First Name"
+                  id="nom"
+                  label="Nom du produit"
                   autoFocus
                 />
               </Grid>
@@ -65,39 +65,50 @@ export default function SignUp() {
                 <TextField
                   required
                   fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
+                  id="type"
+                  label="Type du produit"
+                  name="type"
                   autoComplete="family-name"
                 />
               </Grid>
-              <Grid item xs={12}>
+              <Grid item xs={12} sm={6}>
                 <TextField
                   required
                   fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
+                  id="prix"
+                  label="Prix du produit"
+                  name="prix"
+                  autoComplete="family-name"
                 />
               </Grid>
-              <Grid item xs={12}>
+              <Grid item xs={12} sm={6}>
                 <TextField
                   required
                   fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
+                  id="note"
+                  label="Note du produit"
+                  name="note"
+                  autoComplete="family-name"
                 />
               </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="I want to receive inspiration, marketing promotions and updates via email."
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="garantie"
+                  label="Garantie"
+                  name="garantie"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="disponibilite"
+                  label="Disponibilité"
+                  name="disponibilite"
+                  autoComplete="family-name"
                 />
               </Grid>
             </Grid>
@@ -107,15 +118,8 @@ export default function SignUp() {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign Up
+              Ajouter
             </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
           </Box>
         </Box>
       </Container>
